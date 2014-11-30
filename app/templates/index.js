58bba971abfac43022e83f75573b5cad56850e9d
shgtkshruch <shgtk.shruch@gmail.com>
2014-12-01 07:34:51 +0900

    Routing

--- app/templates/index.js
+++ app/templates/index.js
@@ -3,16 +3,19 @@ var app = koa();
 var views = require('co-views');
 var serve = require('koa-static');
 var stylus = require('koa-stylus');
+var router = require('koa-router');
 
 var render = views('views', {
   ext: 'jade'
 });
 
+app.use(router(app));
+
 app.use(stylus('public'));
 
 app.use(serve('public'));
 
-app.use(function *() {
+app.get('/', function *() {
   this.body = yield render('index');
 });
 
