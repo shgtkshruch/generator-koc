904f7f2e2ab10afcd2f5b489c66df743bc069fbb
shgtkshruch <shgtk.shruch@gmail.com>
2014-12-07 09:04:46 +0900

    Add browserify task.

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -1,15 +1,29 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
+var browserify = require('browserify');
+var transform = require('vinyl-transform');
 
 gulp.task('style', function () {
-  gulp.src('public/style/main.scss')
+  gulp.src('src/style/main.scss')
     .pipe($.plumber())
     .pipe($.sass())
     .pipe(gulp.dest('public/style'));
 });
 
+gulp.task('script', function () {
+  var browserified = transform(function (filename) {
+    var b = browserify(filename);
+    return b.bundle();
+  });
+
+  gulp.src('./src/script/index.js')
+    .pipe($.plumber())
+    .pipe(browserified)
+    .pipe(gulp.dest('public/script'));
+});
+
 gulp.task('nodemon', function () {
-  nodemon({
+  $.nodemon({
     script: 'index.js',
     execMap: {
       js: "node --harmony"
@@ -25,7 +39,11 @@ gulp.task('nodemon', function () {
 });
 
 gulp.task('default', ['nodemon'], function () {
-  $.watch('public/style/**/*.scss', function () {
+  $.watch('src/style/**/*.scss', function () {
     gulp.start('style');
+  });
+
+  $.watch('src/script/**/*.js', function () {
+    gulp.start('script');
   });
 });
